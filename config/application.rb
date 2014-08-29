830385c6c9818e528bf6d8a389dc594061fb700c
Ashley Qian <ashley.qian@duke.edu>
2014-08-29 10:54:42 -0400

    static assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+config.serve_static_assets = true
+
 module Hackduke
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
