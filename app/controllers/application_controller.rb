280089e31fc753d8b9d9cafa1a72c08485ecd86a
Ashley Qian <ashley.qian@duke.edu>
2014-10-06 21:06:37 -0400

    added a helper function in the controller to help render entries only from a specific semester instead of weeding them out in the view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def match_semester(matchable_array, season, year)
+  	matched_model = Array.new
+  	matchable_array.each do |model|
+  		if model.match?(season, year)
+  			matched_model << model
+  		end
+  	end
+  	return matched_model
+  end
 end
--- app/controllers/fall2014/static_controller.rb
+++ app/controllers/fall2014/static_controller.rb
@@ -2,6 +2,7 @@ class Fall2014::StaticController < ApplicationController
 	layout 'fall2014'
 
 	def index
+		@Faq = match_semester(Faq.all, "Fall", 2014)
 		render 'home'
 	end
 
