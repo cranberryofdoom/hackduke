e8e7a76ec7d1386481f7629bbe26da395ac0e935
Ashley Qian <ashley.qian@duke.edu>
2014-10-14 16:02:10 -0400

    typo in prizes controller

--- app/controllers/fall2014/prizes_controller.rb
+++ app/controllers/fall2014/prizes_controller.rb
@@ -2,6 +2,6 @@ class Fall2014::PrizesController < ApplicationController
   layout 'fall2014'
   
   def index
-    @prizes = match_semester(Prizes.all, "Fall", 2014)
+    @prizes = match_semester(Prize.all, "Fall", 2014)
   end
 end
