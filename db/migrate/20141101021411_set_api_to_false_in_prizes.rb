ff6b1c86d5ab8d3022f6b1434efeaa0fed637ed4
Ashley Qian <ashley.qian@duke.edu>
2014-10-31 22:59:12 -0400

    updated all prizes without api attributes at first to false

--- db/migrate/20141101021411_set_api_to_false_in_prizes.rb
+++ db/migrate/20141101021411_set_api_to_false_in_prizes.rb
@@ -2,7 +2,7 @@ class SetApiToFalseInPrizes < ActiveRecord::Migration
   def change
   	Prize.all.each do |p|
   		if p.api == nil
-  			p.api = false
+  			p.update api: false
   		end
   	end
   end
