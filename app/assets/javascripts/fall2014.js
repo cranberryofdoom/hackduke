bdf626afe6b0abe4cd9052309a75f5001704edec
Ashley Qian <ashley.qian@duke.edu>
2014-11-04 18:29:50 -0500

    added video and fixed scrolling

--- app/assets/javascripts/fall2014.js
+++ app/assets/javascripts/fall2014.js
@@ -6,7 +6,7 @@
 //= require angular-application
 
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('.scroll').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
