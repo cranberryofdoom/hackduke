bb422a7ec7c3f79a780091462179398a0e499592
Ashley Qian <ashley.qian@duke.edu>
2014-11-04 15:44:56 -0500

    added scrolling

--- app/assets/javascripts/fall2014.js
+++ app/assets/javascripts/fall2014.js
@@ -4,3 +4,18 @@
 //= require bootstrap
 //= require turbolinks
 //= require angular-application
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
