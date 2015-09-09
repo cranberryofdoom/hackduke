40b4efc6ef67aab7f46705d9a1f11e5fd08b091a
ownzandy <ownz.andy@gmail.com>
2015-09-09 11:01:55 -0400

    rolled back controller years to 2014

--- app/controllers/fall2015/partners_controller.rb
+++ app/controllers/fall2015/partners_controller.rb
@@ -2,6 +2,6 @@ class Fall2015::PartnersController < ApplicationController
   layout 'fall2015'
 
   def index
-    @partners = Partner.joins(:semesters).where({season: "Fall", year: Date.new(2015)})
+    @partners = Partner.joins(:semesters).where({season: "Fall", year: Date.new(2014)})
   end
 end
--- app/controllers/fall2015/prizes_controller.rb
+++ app/controllers/fall2015/prizes_controller.rb
@@ -2,6 +2,6 @@ class Fall2015::PrizesController < ApplicationController
   layout 'fall2015'
   
   def index
-    @prizes = Prize.joins(:semester).where(semesters: {season: "Fall", year: Date.new(2015)})
+    @prizes = Prize.joins(:semester).where(semesters: {season: "Fall", year: Date.new(2014)})
   end
 end
